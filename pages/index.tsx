62a96b64762a1775eca8ad56380aab3d72ef6252
m. nindra zaka <mnindrazaka@gmail.com>
2021-02-28 15:24:49 +0700

    feat: add link to recent works

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,8 +74,8 @@ export default function Home() {
                 I love to learn and teach what I know in the community. I
                 contribute as a meetup speaker, workshop mentor, and
                 facilitator. The amazing thing that I love about teaching is it
-                not only helps others to grow, but it also solidifying what I
-                just learned and deepening my understanding. I create a learning
+                not only helps others to grow, but also solidifying what I just
+                learned and deepening my understanding. I create a learning
                 roadmap that I use to teach about web development
               </Text>
             </Stack>
@@ -85,7 +85,7 @@ export default function Home() {
               _hover={{ textDecoration: "none" }}
             >
               <Button inlineSize="auto" colorScheme="blue">
-                See my teaching roadmap
+                See teaching roadmap
               </Button>
             </Link>
           </div>
@@ -131,46 +131,175 @@ export default function Home() {
 
         <Divider marginY="8" />
 
-        <SimpleGrid columns={4} spacing="8">
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">Lentera</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+        <SimpleGrid columns={3} spacing="8">
+          <Link
+            href="https://lentera.netlify.app/courses/a1630b85-88a2-4b5d-81ef-84537263aba7/workshops/9858ac2b-1852-4b36-99e1-6f015b081629"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">Lentera</Heading>
+              <Text>
+                Online course platform that will give you a guided struggle to
+                learn engineering skill faster and understand better
+              </Text>
+            </Box>
+          </Link>
 
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">Social Media Clone</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+          <Link
+            href="https://code-send.now.sh/"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">Code Send</Heading>
+              <Text>Hot code update platform</Text>
+            </Box>
+          </Link>
 
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">CRUDONE</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+          <Link
+            href="http://wri.polinema.ac.id/"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">WRI Website</Heading>
+              <Text>
+                Community Profile Website of Workshop & Riset Informatika
+              </Text>
+            </Box>
+          </Link>
 
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">useAsync</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+          <Link
+            href="https://www.npmjs.com/package/crudone"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">crudone</Heading>
+              <Text>crud ? done ! react component for easy crud</Text>
+            </Box>
+          </Link>
 
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">useFetch</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+          <Link
+            href="https://github.com/wrideveloper/crew-monitoring"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">Crew Monitoring</Heading>
+              <Text>Community Crew Monitoring</Text>
+            </Box>
+          </Link>
 
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">useLocalStorageState</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+          <Link
+            href="https://www.npmjs.com/package/react-async-handler"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">react-async-handler</Heading>
+              <Text>React hook for handling asynchronous task</Text>
+            </Box>
+          </Link>
 
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">useDebouncedEffect</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+          <Link
+            href="https://www.npmjs.com/package/react-debounce-effect"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">react-debounce-effect</Heading>
+              <Text>React use effect hook but with debounce</Text>
+            </Box>
+          </Link>
 
-          <Box borderWidth="thin" borderRadius="lg" padding="8">
-            <Heading size="md">useToggleState</Heading>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </Box>
+          <Link
+            href="https://www.npmjs.com/package/react-local-storage-state"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">react-local-storage-state</Heading>
+              <Text>
+                React hook for persisting React state to local storage
+              </Text>
+            </Box>
+          </Link>
+
+          <Link
+            href="https://www.npmjs.com/package/react-switch-state"
+            target="_blank"
+            _hover={{ textDecoration: "none" }}
+            borderRadius="lg"
+          >
+            <Box
+              borderWidth="thin"
+              borderRadius="lg"
+              padding="8"
+              width="100%"
+              height="100%"
+            >
+              <Heading size="md">react-switch-state</Heading>
+              <Text>React hook for switching state on and off</Text>
+            </Box>
+          </Link>
         </SimpleGrid>
       </Box>
 
@@ -183,55 +312,89 @@ export default function Home() {
             Want to know more ? Lets have a chat
           </Heading>
           <Stack direction="row" spacing={8} justifyContent="center">
-            <IconButton
+            <Link
+              href="mailto:mnindrazaka@gmail.com"
+              target="_blank"
+              _hover={{ textDecoration: "none" }}
               borderRadius="full"
-              _hover={{ color: "black", backgroundColor: "white" }}
-              borderColor="white"
-              textColor="white"
-              variant="outline"
-              aria-label="gmail"
-              icon={<FaEnvelope />}
-            />
+            >
+              <IconButton
+                borderRadius="full"
+                _hover={{ color: "black", backgroundColor: "white" }}
+                borderColor="white"
+                textColor="white"
+                variant="outline"
+                aria-label="gmail"
+                icon={<FaEnvelope />}
+              />
+            </Link>
 
-            <IconButton
+            <Link
+              href="https://www.linkedin.com/in/mnindrazaka/"
+              target="_blank"
+              _hover={{ textDecoration: "none" }}
               borderRadius="full"
-              _hover={{ color: "black", backgroundColor: "white" }}
-              borderColor="white"
-              textColor="white"
-              variant="outline"
-              aria-label="linkedin"
-              icon={<FaLinkedin />}
-            />
+            >
+              <IconButton
+                borderRadius="full"
+                _hover={{ color: "black", backgroundColor: "white" }}
+                borderColor="white"
+                textColor="white"
+                variant="outline"
+                aria-label="linkedin"
+                icon={<FaLinkedin />}
+              />
+            </Link>
 
-            <IconButton
+            <Link
+              href="https://github.com/mnindrazaka"
+              target="_blank"
+              _hover={{ textDecoration: "none" }}
               borderRadius="full"
-              _hover={{ color: "black", backgroundColor: "white" }}
-              borderColor="white"
-              textColor="white"
-              variant="outline"
-              aria-label="github"
-              icon={<FaGithub />}
-            />
+            >
+              <IconButton
+                borderRadius="full"
+                _hover={{ color: "black", backgroundColor: "white" }}
+                borderColor="white"
+                textColor="white"
+                variant="outline"
+                aria-label="github"
+                icon={<FaGithub />}
+              />
+            </Link>
+            <Link
+              href="https://twitter.com/mnindrazaka"
+              target="_blank"
+              _hover={{ textDecoration: "none" }}
+              borderRadius="full"
+            >
+              <IconButton
+                borderRadius="full"
+                _hover={{ color: "black", backgroundColor: "white" }}
+                borderColor="white"
+                textColor="white"
+                variant="outline"
+                aria-label="twitter"
+                icon={<FaTwitter />}
+              />
+            </Link>
 
-            <IconButton
+            <Link
+              href="https://www.facebook.com/mnindrazaka/"
+              target="_blank"
+              _hover={{ textDecoration: "none" }}
               borderRadius="full"
-              _hover={{ color: "black", backgroundColor: "white" }}
-              borderColor="white"
-              textColor="white"
-              variant="outline"
-              aria-label="twitter"
-              icon={<FaTwitter />}
-            />
-
-            <IconButton
-              borderRadius="full"
-              _hover={{ color: "black", backgroundColor: "white" }}
-              borderColor="white"
-              textColor="white"
-              variant="outline"
-              aria-label="facebook"
-              icon={<FaFacebook />}
-            />
+            >
+              <IconButton
+                borderRadius="full"
+                _hover={{ color: "black", backgroundColor: "white" }}
+                borderColor="white"
+                textColor="white"
+                variant="outline"
+                aria-label="facebook"
+                icon={<FaFacebook />}
+              />
+            </Link>
           </Stack>
         </Stack>
       </Box>
