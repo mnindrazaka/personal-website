e07e5e8ce003320c0386ece6e23e8ffdad28cbc9
m. nindra zaka <mnindrazaka@gmail.com>
2021-03-02 07:18:56 +0700

    chore: redirect to blog subdomain

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    target: "serverless"
+    target: "serverless",
+    async redirects() {
+        return [
+          {
+            source: '/:slug',
+            destination: 'https://blog.mnindrazaka.com/:slug',
+            permanent: true,
+          },
+        ]
+      },
 }
